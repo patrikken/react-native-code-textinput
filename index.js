fbb9b3549170fd5e70943cb09e177c00e3544736
patrik_ken <kenfackjoslin@gmail.com>
2020-06-09 00:07:04 +0300

    update index

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-/**
- * @format
- */
+/* 
+import { AppRegistry } from 'react-native';
+import App from './exemple/App';
+import { name as appName } from './app.json';
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+AppRegistry.registerComponent(appName, () => App); */
 
-AppRegistry.registerComponent(appName, () => App);
+//import './src/AppInput'
+import CodeInput from './src/CodeInput';
+
+export default CodeInput;
